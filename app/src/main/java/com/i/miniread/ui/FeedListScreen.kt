0f3b7a4ef9b67885e1ada6b5762a2f1f0fde5bdc
BonexP <52726573+BonexP@users.noreply.github.com>
2024-08-19 17:45:42 +0800

    Add more function.update dependency

--- app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
+++ app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
@@ -2,10 +2,18 @@ package com.i.miniread.ui
 
 import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -21,10 +29,13 @@ fun FeedListScreen(viewModel: MinifluxViewModel, navController: NavController) {
 
     Log.d("FeedListScreen", "Number of feeds: ${feeds.size}")
 
-    LazyColumn(modifier = Modifier.padding(16.dp)) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         items(feeds) { feed ->
             FeedItem(feed, onClick = {
-                // Handle click event, navigate to article detail screen
                 viewModel.setSelectedEntry(feed)
                 navController.navigate("articleDetail")
             })
@@ -37,13 +48,22 @@ fun FeedItem(feed: Entry, onClick: () -> Unit) {
     val title = feed.title ?: "Untitled"
     val publishedAt = feed.published_at ?: "Unknown Date"
 
-    Column(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clickable { onClick() }
+            .padding(vertical = 8.dp)
+            .clickable { onClick() },
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
     ) {
-        Text(text = title, style = MaterialTheme.typography.headlineLarge)
-        Text(text = publishedAt, style = MaterialTheme.typography.bodyLarge)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = title, style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = publishedAt, style = MaterialTheme.typography.bodyMedium)
+        }
     }
 }
