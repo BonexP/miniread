0cb0f327a3be3b99222bae81826abbd9c330274b
BonexP <52726573+BonexP@users.noreply.github.com>
2024-08-19 15:26:32 +0800

    login with API token

--- app/src/main/java/com/i/miniread/network/MinifluxApi.kt
+++ app/src/main/java/com/i/miniread/network/MinifluxApi.kt
@@ -5,13 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
 interface MinifluxApi {
-    @POST("v1/authentication")
-    @FormUrlEncoded
-    suspend fun login(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): AuthResponse
-
     @GET("v1/feeds")
     suspend fun getFeeds(@Header("X-Auth-Token") authToken: String): List<Entry>
 
@@ -19,14 +12,13 @@ interface MinifluxApi {
     suspend fun getCategories(@Header("X-Auth-Token") authToken: String): List<Category>
 }
 
-data class AuthResponse(val token: String)
 data class Entry(val id: Int, val title: String, val url: String, val content: String, val published_at: String)
 data class Category(val id: Int, val title: String)
 
 object RetrofitInstance {
     val api: MinifluxApi by lazy {
         Retrofit.Builder()
-            .baseUrl("https://lifeo3.icu:4081/api/")  // 将其替换为您实际的 Miniflux API URL
+            .baseUrl("https://lifeo3.icu:4081/")  // 替换为实际的 Miniflux API URL
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MinifluxApi::class.java)
