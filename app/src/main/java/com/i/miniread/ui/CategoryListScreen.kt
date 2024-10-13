ffc19fb8940b5c7e9e73f47f3c6fe4a45f0197ec
CeonDuna <CeonDuna@z66.local>
2024-10-13 22:43:38 +0800

    add colorScheme,add border

--- app/src/main/java/com/i/miniread/ui/CategoryListScreen.kt
+++ app/src/main/java/com/i/miniread/ui/CategoryListScreen.kt
@@ -1,19 +1,24 @@
 package com.i.miniread.ui
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.i.miniread.network.Category
 import com.i.miniread.viewmodel.MinifluxViewModel
@@ -57,8 +62,11 @@ fun CategoryItem(category: Category, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
+            .border(BorderStroke(0.5.dp, Color.Black), shape = RoundedCornerShape(8.dp))
             .clickable { onClick() },
-    ) {
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+
+        ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(
                 text = category.title,
--- app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
+++ app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
@@ -1,6 +1,8 @@
 package com.i.miniread.ui
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +12,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.i.miniread.network.Feed
 import com.i.miniread.viewmodel.MinifluxViewModel
@@ -53,9 +58,10 @@ fun FeedItem(feed: Feed, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
+            .border(BorderStroke(0.5.dp, Color.Black), shape = RoundedCornerShape(8.dp))
             .clickable { onClick() },
 //        shape = MaterialTheme.shapes.medium,
-//        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
     ) {
         Column(
             modifier = Modifier
