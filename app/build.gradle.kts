b4275fe8a5c03a647bfc37be8ffd940f8b2b2931
BonexP <52726573+BonexP@users.noreply.github.com>
2024-11-09 19:36:58 +0800

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 }
 val versionname by extra("dev")
-val versioncode by extra("0.0.15")
-val versionnumber by extra(15)
+val versioncode by extra("0.0.16")
+val versionnumber by extra(16)
 
 android {
     namespace = "com.i.miniread"
