3f81c6d7e5f1bd8eaf96b64a578c16bfddc70f15
BonexP <52726573+BonexP@users.noreply.github.com>
2024-12-04 23:09:19 +0800

    reformatted code

--- app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
+++ app/src/main/java/com/i/miniread/ui/FeedListScreen.kt
@@ -5,19 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -29,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.google.android.datatransport.runtime.firebase.transport.LogEventDropped
 import com.i.miniread.network.Feed
 import com.i.miniread.viewmodel.MinifluxViewModel
 
@@ -69,7 +63,7 @@ fun FeedListScreen(
                 FeedItem(feed, onClick = {
                     Log.d("FeedListScreen", "Feed Using:  $feed")
                     onFeedSelected(feed.id)
-                },onMarkAsRead={
+                }, onMarkAsRead = {
                     viewModel.markFeedAsRead(feed.id)
                     Log.d("FeedList", "FeedListScreen: invoke onMarkAsRad in FeedlistScreen!")
                 })
@@ -79,7 +73,7 @@ fun FeedListScreen(
 }
 
 @Composable
-fun FeedItem(feed: Feed, onClick: () -> Unit,onMarkAsRead: () -> Unit) {
+fun FeedItem(feed: Feed, onClick: () -> Unit, onMarkAsRead: () -> Unit) {
     val title = feed.title
     val id = feed.id
 
