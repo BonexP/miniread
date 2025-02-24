b696824749d1c186ab741e6fa6419698d2ef423e
BonexP <52726573+BonexP@users.noreply.github.com>
2025-02-24 20:58:06 +0800

    build: 更新应用版本信息
    - 将版本号从 0.0.17 修改为 0.0.18
    -

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 }
 val versionname by extra("dev")
-val versioncode by extra("0.0.17")
-val versionnumber by extra(17)
+val versioncode by extra("0.0.18")
+val versionnumber by extra(18)
 
 android {
     namespace = "com.i.miniread"
