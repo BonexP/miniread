530a8ed9074982e89a25301ae225854f5ba72925
oem7020 <oem7020@OP7020>
2025-04-20 18:42:23 +0800

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 }
 val versionname by extra("dev")
-val versioncode by extra("0.0.18")
-val versionnumber by extra(18)
+val versioncode by extra("0.0.19")
+val versionnumber by extra(19)
 
 android {
     namespace = "com.i.miniread"
