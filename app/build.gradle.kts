bb2525de3bf1a8b744ff55ef38125a455ab33ce9
CeonDuna <CeonDuna@z66.local>
2025-10-22 20:20:32 +0800

    Bump version code to 0.2.1 and version number to 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.ksp)
 }
 val versionname by extra("dev")
-val versioncode by extra("0.2.0")
-val versionnumber by extra(24)
+val versioncode by extra("0.2.1")
+val versionnumber by extra(25)
 
 android {
     namespace = "com.i.miniread"
