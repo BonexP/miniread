e91837992a8f7b06e83229cf58d86137a6d35ea5
oem7020 <oem7020@OP7020>
2025-10-26 18:42:01 +0800

    chore: update version code and number to 0.3.0a and 28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.ksp)
 }
 val versionname by extra("dev")
-val versioncode by extra("0.3.0")
-val versionnumber by extra(27)
+val versioncode by extra("0.3.0a")
+val versionnumber by extra(28)
 
 android {
     namespace = "com.i.miniread"
