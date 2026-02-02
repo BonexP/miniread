4e0ed549212c80e99255bd9632aec47300c938c9
BonexP <52726573+BonexP@users.noreply.github.com>
2026-02-03 00:58:43 +0800

    Revert commits after ed18555: rollback to stable version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.ksp)
 }
 val versionname by extra("dev")
-val versioncode by extra("0.3.4")
-val versionnumber by extra(33)
+val versioncode by extra("0.3.3")
+val versionnumber by extra(32)
 
 android {
     namespace = "com.i.miniread"
